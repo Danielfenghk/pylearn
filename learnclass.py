78d205cefba93a29ce1bd52a628d3fe392129510
danielslab <ggfeng@hotmail.com>
2018-02-12 09:15:42 +0800

    change on master

--- learnclass.py
+++ learnclass.py
@@ -2,7 +2,8 @@ class RevealAccess(object):
     """A data descriptor that sets and returns values
        normally and prints a message logging their access.
     """
-
+#add first line
+#add second line
     def __init__(self, initval=None, name='var'):
         self.val = initval
         self.name = name
