74ef081cd0c30aa2d51e59e810990102fb823ce1
danielslab <ggfeng@hotmail.com>
2018-02-12 13:03:27 +0800

    add by dev4

--- learnclass.py
+++ learnclass.py
@@ -2,11 +2,20 @@ class RevealAccess(object):
     """A data descriptor that sets and returns values
        normally and prints a message logging their access.
     """
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 =======
 >>>>>>> add second line
+<<<<<<< HEAD
 >>>>>>> no message
+=======
+=======
+#this is a comment
+#add sencond line
+#dev4 add
+>>>>>>> add by dev4
+>>>>>>> add by dev4
     def __init__(self, initval=None, name='var'):
         self.val = initval
         self.name = name
